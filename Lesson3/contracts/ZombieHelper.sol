72ec842bef3dd298fc2c5d1736775fa00c681a37
Rex Hygate <rex@ethereumusecases.com>
2018-11-20 21:24:02 -0500

    Change req syntax on Lesson 3

--- Lesson3/contracts/ZombieHelper.sol
+++ Lesson3/contracts/ZombieHelper.sol
@@ -14,21 +14,21 @@ contract ZombieHelper is ZombieFeeding {
       require(zombies[_zombieId].level >= _level);
       _;
     }
-// req C3_5 Changes the name of a zombie
-// req C3_6 Name change can only be done by the zombie's owner
+// req ZH_5 Changes the name of a zombie
+// req ZH_6 Name change can only be done by the zombie's owner
     function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
         require(msg.sender == zombieToOwner[_zombieId]);
         // Check length of name
         zombies[_zombieId].name = _newName;
     }
-// C3_7 Changes the dna of a zombie if it is above level 20
-// C3_8 Only the owner of a zombie can change its dna
+// ZH_7 Changes the dna of a zombie if it is above level 20
+// ZH_8 Only the owner of a zombie can change its dna
     function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
         require(msg.sender == zombieToOwner[_zombieId]);
         zombies[_zombieId].dna = _newDna;
     }
 
-// C3_9 Create and return an array of the number of zombies by owner for use in other function
+// ZH_9 Create and return an array of the number of zombies by owner for use in other function
     function getZombiesByOwner(address _owner) external view returns(uint[]) {
         uint[] memory result = new uint[](ownerZombieCount[_owner]);
         uint counter = 0;
